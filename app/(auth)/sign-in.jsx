cfe63b992e6058f76dd10718193117c99f15232b
capmahin <mahinulhuque1996@gmail.com>
2025-07-11 20:24:09 +0600

    add if statement

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,7 +1,23 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Alert } from "react-native";
+import { useSignIn } from "@clerk/clerk-expo";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
 
 const SignInScreen = () => {
+  const router = useRouter();
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+  const [ShowPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSignIn = async () => {
+    if (!email || !password) {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+  };
+
   return (
     <View>
       <Text>Sign In</Text>

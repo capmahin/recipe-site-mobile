8488e14b96630d979f3dbc21704f06d1fec4aa8c
capmahin <mahinulhuque1996@gmail.com>
2025-07-12 13:45:18 +0600

    add handler

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
 import React, { useState } from "react";
 import { useRouter } from "expo-router";
 import { useSignUp } from "@clerk/clerk-expo";
@@ -11,6 +11,13 @@ const SignUpScreen = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [loading, setLoading] = useState(false);
   const [pendingVerificartion, setPendingVerification] = useState(false);
+
+  const handleSignUp = async () => {
+    if (!email || !password)
+      return Alert.alert("Error", "Please fill in all fields");
+    if (password.length < 6)
+      return Alert.alert("Error", "Password must be at least 6 characters");
+  };
 
   return (
     <View>

dcb2c3c33e20bd640348b41f725c2767f0106ef9
capmahin <mahinulhuque1996@gmail.com>
2025-07-12 13:10:20 +0600

    add usestates for email,password,showPassword,loaded etc

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,7 +1,17 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import { useSignUp } from "@clerk/clerk-expo";
 
 const SignUpScreen = () => {
+  const router = useRouter();
+  const { isLoaded, signUp } = useSignUp();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [pendingVerificartion, setPendingVerification] = useState(false);
+
   return (
     <View>
       <Text>SignUpScreen</Text>

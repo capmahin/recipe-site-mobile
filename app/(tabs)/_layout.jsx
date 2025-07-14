aa99b961fe172917e821dd6f813a70c3dc5ed9e0
capmahin <mahinulhuque1996@gmail.com>
2025-07-15 02:28:26 +0600

    add tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Redirect, Stack } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
 
 const TabsLayout = () => {
-  const { isSignedIn } = useAuth();
+  // const { isSignedIn } = useAuth();
 
-  if (!isSignedIn) return <Redirect href={"/(auth)/sign-in"} />;
-  return <Stack />;
+  // if (!isSignedIn) return <Redirect href={"/(auth)/sign-in"} />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Recipes",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="restaurant" size={size} color={color} />
+          )
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default TabsLayout;
